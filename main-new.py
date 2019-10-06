a4c6b0caad3d615a6869361c6613dfa4254a134d
Volodymyr Lapada <volodymyr.lapada@tomtom.com>
2019-10-06 19:00:52 +0200

    updated map from drawings

--- main-new.py
+++ main-new.py
@@ -1,4 +1,5 @@
 import sys, pygame, time
+from random import randrange
 pygame.init()
 pygame.font.init()
 
@@ -37,10 +38,12 @@ class TPlayer:
         self.image = self.ImageLoad(ItemImage)
         self.speed = speed
         self.screen = screen
-        self.PlayerX = 1
-        self.PlayerY = 1
+        self.PlayerX = 0
+        self.PlayerY = 0
         self.PlayerIsoX = 0
         self.PlayerIsoY = 0
+        self.screenOffsetX = 9
+        self.screenOffsetY = 1
         self.Health = 60
         self.MaxHealth = 60
         self.Armor = 0
@@ -97,11 +100,11 @@ class TCollectionItem:
     def __init__(self, LevelArray, PickItemsArray):
         self.BoardArray =[[]]
         self.PItemsArray =[[]]
-        self.size = x, y = 1280, 800
-        self.tilesize = 64
+        self.size = x, y = 640, 480
+        self.tilesize = 12
         self.black = 0, 0, 0
         self.screen = pygame.display.set_mode(self.size)
-        self.Walls = [ TItem('floor', 'tile.png', screen = self.screen), TItem('wall', 'block1.png', 32, screen = self.screen), TItem('wall', 'piramid.png', screen = self.screen), TItem('wall', 'piramid1.png', 32, screen = self.screen) ]
+        self.Walls = [ TItem('floor', 'room1_floor_tile24x12_1.png', screen = self.screen), TItem('floor', 'room1_floor_tile24x12_2.png', screen = self.screen), TItem('floor', 'room1_floor_tile24x12_3.png', screen = self.screen), TItem('wall', 'room1_table1_24x34_p1.png', 22, screen = self.screen), TItem('wall', 'room1_table1_24x34_p2.png', 22, screen = self.screen), TItem('wall', 'room1_table1_24x34_p3.png', 22, screen = self.screen), TItem('wall', 'room1_table1_24x34_p3.png', 16, screen = self.screen), ]
         self.Items = [ TItem('pants', 'pants.png', screen = self.screen, properties = {'armor': 2, 'durability': '10'})]
         self.Player = TPlayer('p1.png', 1, screen = self.screen, BoardArray = self.BoardArray)
         self.CreateBoard(LevelArray)
@@ -121,37 +124,41 @@ class TCollectionItem:
                     self.BoardArray[i].append(self.Walls[2])
                 elif ConfigBoardArray[i][j] == 3:
                     self.BoardArray[i].append(self.Walls[3])
+                elif ConfigBoardArray[i][j] == 4:
+                    self.BoardArray[i].append(self.Walls[4])
+                elif ConfigBoardArray[i][j] == 5:
+                    self.BoardArray[i].append(self.Walls[5])
+                elif ConfigBoardArray[i][j] == 6:
+                    self.BoardArray[i].append(self.Walls[6])
 
     def CreatePickItems(self, ItemsArray):
-        print ItemsArray
         maxX = len(ItemsArray[1])
         maxY = len(ItemsArray[0])
         for i in range(maxY):
             self.PItemsArray.append([])
             for j in range(maxX):
-                print "i={0} j={1}".format(i,j)
-                print self.PItemsArray
-                print "Array{0},{1} = {2}".format(i,j,ItemsArray[i][j])
                 if ItemsArray[i][j] == 0:
                     self.PItemsArray[i].append(self.Items[0])
                 else:
                     self.PItemsArray[i].append(None)
-
     def ReDrawScreen(self):
         self.screen.fill(self.black)
         PlayerCoord = self.toiso(self.Player.PlayerX, self.Player.PlayerY)
-        self.Player.PlayerIsoX = PlayerCoord["x"]+9*64+64
-        self.Player.PlayerIsoY = PlayerCoord["y"]+1*64
+        self.Player.PlayerIsoX = PlayerCoord["x"]+self.Player.screenOffsetX*self.tilesize+self.tilesize/2
+        self.Player.PlayerIsoY = PlayerCoord["y"]-self.tilesize*2+self.tilesize/2
         for i in range(10):
             for j in range(10):
                 DrawCoord = self.toiso(i,j)
-                if j == self.Player.PlayerY and i == self.Player.PlayerX:
-                    self.BoardArray[j][i].Draw((DrawCoord["x"]+9*64, DrawCoord["y"]+1*64))
-                    self.Player.Draw()
+                if self.BoardArray[j][i].kind == 'wall':
+                    self.Walls[2].Draw((DrawCoord["x"]+self.Player.screenOffsetX*self.tilesize, DrawCoord["y"]+self.Player.screenOffsetY*self.tilesize))
                 else:
-                    self.BoardArray[j][i].Draw((DrawCoord["x"]+9*64, DrawCoord["y"]+1*64))
-                if self.PItemsArray[j][i] != None:
-                    self.PItemsArray[j][i].Draw((DrawCoord["x"]+9*64, DrawCoord["y"]+1*64))
+                    self.BoardArray[j][i].Draw((DrawCoord["x"]+self.Player.screenOffsetX*self.tilesize, DrawCoord["y"]+self.Player.screenOffsetY*self.tilesize))
+        for i in range(10):
+            for j in range(10):
+                DrawCoord = self.toiso(i,j)
+                if self.BoardArray[j][i].kind == 'wall':
+                    self.BoardArray[j][i].Draw((DrawCoord["x"]+self.Player.screenOffsetX*self.tilesize, DrawCoord["y"]+self.Player.screenOffsetY*self.tilesize))
+        self.Player.Draw()
         self.Player.ShowPos()
         pygame.display.flip()
 
@@ -161,16 +168,16 @@ class TCollectionItem:
         res["y"] = (x*self.tilesize + y*self.tilesize)/2
         return res
        
-levelArr = [[0,1,1,1,1,1,1,1,1,1],
-            [1,0,0,0,0,0,0,0,0,1],
-            [1,0,0,1,1,1,1,2,0,1],
-            [1,0,0,1,0,0,0,1,0,1],
-            [1,0,0,1,3,3,0,1,0,1],
-            [1,0,0,0,0,1,0,1,0,1],
-            [1,0,2,0,0,0,0,1,0,1],
-            [1,0,0,0,0,1,1,1,0,1],
-            [1,0,0,2,0,0,0,0,0,3],
-            [1,1,1,1,1,1,1,1,1,1]]
+levelArr = [[0,1,1,0,2,0,1,2,1,0],
+            [2,1,1,2,0,1,2,0,2,2],
+            [0,0,0,2,2,0,2,0,0,1],
+            [2,1,0,1,1,2,1,0,0,2],
+            [0,1,2,1,0,0,1,2,1,1],
+            [2,1,1,0,1,2,2,2,1,1],
+            [1,1,2,1,0,5,0,1,2,2],
+            [2,0,1,0,0,4,0,2,1,0],
+            [0,1,1,0,2,3,0,0,1,2],
+            [0,2,0,1,1,0,2,0,0,0]]
 itemsArr = [[1,1,1,1,1,1,1,1,1,1],
             [1,0,0,1,1,1,1,1,1,1],
             [1,1,1,1,1,1,1,1,1,1],
